5b2212038ef1f162ba5ec97b52b52d0f98f144a7
shreyal0007 <shreyaljain0007@gmail.com>
2023-03-12 15:18:20 +0530

    initialized the web

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const config = {
+  SUPABASE_URL: "https://bwqibqbxbirzbyjqovco.supabase.co",
+  SUPABASE_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsdXVub3BmdXdobGxwa2R1eXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc0OTI0MzcsImV4cCI6MTk5MzA2ODQzN30.Jv8d446Xege5vjbO12VINANIXbeqk1DyrA7mtVIqVwA",
+};
+export default config;
